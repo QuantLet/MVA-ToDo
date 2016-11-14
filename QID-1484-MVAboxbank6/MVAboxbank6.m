b09f8bf4473922f749b16e3269861acf8e0f0615
Lily <lilymiru@gmail.com>
2016-11-14 14:20:02 +0100

    check Matlab vs R

--- QID-1484-MVAboxbank6/MVAboxbank6.m
+++ QID-1484-MVAboxbank6/MVAboxbank6.m
@@ -1,33 +1,14 @@
-% ---------------------------------------------------------------------
-% Book:         MVA
-% ---------------------------------------------------------------------
-% Quantlet:     MVAboxbank6
-% ---------------------------------------------------------------------
-% Description:  MVAboxbank6 computes Boxplots for the diagonal
-%               (Variable 6) of the genuine and forged banknotes from
-%               bank2.dat, respectively.
-% ---------------------------------------------------------------------
-% Usage:        -
-% ---------------------------------------------------------------------
-% Inputs:       none
-% ---------------------------------------------------------------------
-% Output:       Boxplots for the diagonal (Variable 6)
-%               of the genuine and forged banknotes from bank2.dat,
-%               respectively.
-% ---------------------------------------------------------------------
-% Example:
-% ---------------------------------------------------------------------
-% Author:       Wolfgang Haerdle, Vladimir Georgescu, Jorge Patron,
-%               Song Song
-% ---------------------------------------------------------------------
-
+% clear variables and close windows
 clear
 close all
 clc
 
-x=load('bank2.dat');
-m1=mean(x(1:100,6))
-m2=mean(x(101:200,6))
+% load data
+x  = load('bank2.dat');
+m1 = mean(x(1:100,6))
+m2 = mean(x(101:200,6))
+
+% plot
 hold on
 line([0.6 1.4],[m1 m1],'Color','k','LineStyle',':','LineWidth',1.2)
 line([1.6 2.4],[m2 m2],'Color','k','LineStyle',':','LineWidth',1.2)
--- QID-2428-MVAboxbank1/MVAboxbank1.m
+++ QID-2428-MVAboxbank1/MVAboxbank1.m
@@ -1,32 +1,14 @@
-% -------------------------------------------------------------------------
-% Book:         MVA
-% -------------------------------------------------------------------------
-% Quantlet:     MVAboxbank1
-% -------------------------------------------------------------------------
-% Description:  MVAboxbank1 computes Boxplots for the length (Variable 1)
-%               of the genuine and forged banknotes from bank2.dat,
-%               respectively.
-% -------------------------------------------------------------------------
-% Usage:        -
-% -------------------------------------------------------------------------
-% Inputs:       none
-% -------------------------------------------------------------------------
-% Output:       Boxplots for the length (Variable 1) of the genuine and
-%               forged banknotes from bank2.dat, respectively.
-% -------------------------------------------------------------------------
-% Example:
-% -------------------------------------------------------------------------
-% Author:       Wolfgang Haerdle, Vladimir Georgescu, Jorge Patron, Song 
-%               Song
-% -------------------------------------------------------------------------
-
+% clear variables and close windows
 close all
 clear
 clc
 
-x=load('bank2.dat');
-m1=mean(x(1:100,1));
-m2=mean(x(101:200,1));
+%  load data
+x  = load('bank2.dat');
+m1 = mean(x(1:100,1));
+m2 = mean(x(101:200,1));
+
+%  plot
 hold on
 line([0.6 1.4],[m1 m1],'Color','k','LineStyle',':','LineWidth',1.2)
 line([1.6 2.4],[m2 m2],'Color','k','LineStyle',':','LineWidth',1.2)
